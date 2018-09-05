c51e803f4c355c35a6cd757e8a7d78e5f4bf9939
siegrein <siegrein13@gmail.com>
2018-09-05 22:11:48 +0300

    Added content in UserPage Created methods in UserPageController.java Set up formatting in sample.fxml and signUp.fxml

--- src/sample/Controllers/UserPageController.java
+++ src/sample/Controllers/UserPageController.java
@@ -1,4 +1,44 @@
 package sample.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import sample.PageOpener;
+
+import java.io.IOException;
+
 public class UserPageController {
+
+
+    public void userPageOpenBookListButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void userPageOpenRecentBooksButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void userPageSoonToReadButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void userPageNameTextFieldAction(ActionEvent actionEvent) {
+    }
+
+    public void userPageTelTextFieldAction(ActionEvent actionEvent) {
+    }
+
+    public void userPageChangeMyDataButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void userPageSendLetterButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void userPageAddNewBookButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void userPageGoToBookTopsButtonAction(ActionEvent actionEvent) {
+    }
 }
