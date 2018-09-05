695fd008120af5e37fde4d5d9e1d883cf271b808
Ivan <chupakhin.ivan56.gmail.com>
2018-09-05 19:54:15 +0300

    Rename signUpController -> SignUpController

--- src/sample/Controllers/signUpController.java
+++ src/sample/Controllers/signUpController.java
@@ -1,4 +1,18 @@
 package sample.Controllers;
 
-public class signUpController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import sample.PageOpener;
+
+public class SignUpController {
+
+    @FXML
+    public Button signUpSignInButton;
+
+    public void signUpSignInButtonAction(ActionEvent actionEvent) {
+        signUpSignInButton.getScene().getWindow().hide();
+        PageOpener opener = new PageOpener();
+        opener.openPage("/sample/Interface/sample.fxml");
+    }
 }
