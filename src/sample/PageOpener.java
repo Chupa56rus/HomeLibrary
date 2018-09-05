f5597e3a62733669e31cd4c9d1d5bab04138428b
Ivan <chupakhin.ivan56.gmail.com>
2018-09-05 16:10:19 +0300

    Added Button signInSignUpButton in SignUp Page Added event handler signInSignUpButton and signUpSignInButton Create class PageOpener Rename signUpController -> SignUpController

---
+++ src/sample/PageOpener.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PageOpener {
+
+    public void openPage(String path){
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(path));
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
+    }
+}
