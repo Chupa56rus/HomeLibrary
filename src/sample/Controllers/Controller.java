2062bf83bb3deac56d9e423db769da546fb0ebb9
siegrein <siegrein13@gmail.com>
2018-09-11 12:28:40 +0300

    Added controls for the login/password pair on the SignIn Page

--- src/sample/Controllers/Controller.java
+++ src/sample/Controllers/Controller.java
@@ -3,7 +3,13 @@ package sample.Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import sample.PageOpener;
+import sample.sql.DataBaseConnection;
+
+import java.sql.*;
 
 
 public class Controller {
@@ -12,6 +18,9 @@ public class Controller {
     public Button signInSignUpButton;
     public Button signInSignInButton;
     public Button signInCancelButton;
+    public Label signInErrorMessage;
+    public PasswordField signInPasswordField;
+    public TextField signInLoginField;
 
     public void signInSignUpButtonAction(ActionEvent actionEvent) {
 
@@ -20,10 +29,35 @@ public class Controller {
         opener.openPage("/sample/Interface/signUp.fxml");
     }
 
-    public void signInSignInButtonAction(ActionEvent actionEvent) {
-        signInSignInButton.getScene().getWindow().hide();
-        PageOpener opener = new PageOpener();
-        opener.openPage("/sample/Interface/userPage.fxml");
+    public void signInSignInButtonAction(ActionEvent actionEvent) throws ClassNotFoundException, SQLException {
+        signInErrorMessage.setVisible(false);
+        String userLogin = signInLoginField.getText();
+        String userPassword = signInPasswordField.getText();
+
+        if ((userLogin.isEmpty()) || (userPassword.isEmpty())) {
+            signInErrorMessage.setVisible(true);
+        } else {
+            DataBaseConnection con = new DataBaseConnection();
+            Class.forName("com.mysql.jdbc.Driver");
+            try (Connection connection = DriverManager.getConnection(DataBaseConnection.getUrl(), DataBaseConnection.getUsername(), DataBaseConnection.getPassword());
+                 Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery("select f_user_id, f_user_username, f_user_password from homelib.t_users " +
+                        "where f_user_username = '" + userLogin + "';");
+                resultSet.last();
+                if (resultSet.getRow() < 1) {
+                    signInErrorMessage.setVisible(true);
+                } else if ((resultSet.getString("f_user_username").equals(userLogin))
+                        && (resultSet.getString("f_user_password").equals(userPassword))) {
+                    signInErrorMessage.setVisible(false);
+                    signInSignInButton.getScene().getWindow().hide();
+                    PageOpener opener = new PageOpener();
+                    opener.openPage("/sample/Interface/userPage.fxml");
+                } else {
+                    signInErrorMessage.setVisible(true);
+                }
+            }
+        }
+
     }
 
     public void signInCancelButtonAction(ActionEvent actionEvent) {
